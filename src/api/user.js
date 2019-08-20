5b2714d49fb3b7d6fdc1c16fd23bbee0f4892080
Roger.G <2319743089@qq.com>
2019-08-20 10:14:54 +0800

    修復訪問出錯問題

--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,7 @@ import request from "@/utils/request.js"
 import Qs from "qs"
 export function login(login, password) {
     return request({
-        url: "login",
+        url: "api/login",
         method: "POST",
         data: Qs.stringify({
             login, password
