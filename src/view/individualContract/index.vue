542aa0a7f4ce013ba7874f6365c3b92eeb6bef09
keenchan <keen@ormediagroup.com>
2019-08-30 12:46:28 +0800

    糕

--- src/view/individualContract/index.vue
+++ src/view/individualContract/index.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
     <p class="header">
-      <a-input-search placeholder="search by termContract id" style="width: 200px" @search="onSearch" />
+      <a-input-search
+        placeholder="search by individualContract id"
+        style="width: 200px"
+        @search="onSearch"
+      />
       <span>
         <a-button
           type="primary"
           @click="()=>{
-        this.$refs.newtermContract.show()
+        this.$refs.newindividualContract.show()
         }"
         >新增常規合約</a-button>
       </span>
@@ -14,24 +18,28 @@
     <a-table :columns="columns" :dataSource="tableData" :loading="onTableLoading">
       <template slot="detail" slot-scope="record">
         <a @click="()=>{
-          $refs.edittermContract.show(record)
+          $refs.editindividualContract.show(record)
           }">更多</a>
       </template>
       <template slot="indContract" slot-scope="record">
-        <a @click="()=>{
-          $refs.individualContract.show(record.term_contract_id)
-          }">額外合約</a>
+        <a
+          @click="()=>{
+          $refs.individualContract.show(record.individual_contract_id)
+          }"
+        >額外合約</a>
       </template>
       <template slot="report" slot-scope="record">
-        <a @click="()=>{
-          $refs.regularReport.show(record.term_contract_id)
-          }">提交檢查報告</a>
+        <a
+          @click="()=>{
+          $refs.regularReport.show(record.individual_contract_id)
+          }"
+        >提交檢查報告</a>
       </template>
       <template slot="delete" slot-scope="record">
         <a-popconfirm
           v-if="tableData.length"
           title="Sure to delete?"
-          @confirm="() => onDelete(record.term_contract_id)"
+          @confirm="() => onDelete(record.individual_contract_id)"
         >
           <a>
             <a-icon type="delete"></a-icon>
@@ -39,106 +47,66 @@
         </a-popconfirm>
       </template>
     </a-table>
-    <newtermContract ref="newtermContract" @done="()=>{
+    <newindividualContract
+      ref="newindividualContract"
+      @done="()=>{
       this.getTableData(this.$route.params.bid);
-      }" />
-    <edittermContract ref="edittermContract" @done="()=>{
+      }"
+    />
+    <editindividualContract
+      ref="editindividualContract"
+      @done="()=>{
     this.getTableData(this.$route.params.bid);
-    }" />
-    <individualContract ref="individualContract" @done="()=>{
-    this.getTableData(this.$route.params.bid);
-    }" />
-    <regularReport ref="regularReport" @done="()=>{
-    this.getTableData(this.$route.params.bid);
-    }" />
+    }"
+    />
   </div>
 </template>
 <script>
-import newtermContract from "./new";
-import edittermContract from "./edit";
-import { r_term_contract, d_term_contract } from "@/api/term_contract.js";
+import newindividualContract from "./new";
+import editindividualContract from "./edit";
+import {
+  r_individual_contract,
+  d_individual_contract
+} from "@/api/individual_contract.js";
 import uuiddv1 from "uuid/v1";
 const columns = [
-  { title: "一次性合約編號", dataIndex: "term_contract_id", key: "term_contract_id" },
+  {
+    title: "一次性合約編號",
+    dataIndex: "individual_contract_id",
+    key: "individual_contract_id"
+  },
   { title: "一次性合約種類", width: "150px", dataIndex: "type", key: "type" },
-  { title: "合約期至", dataIndex: "contract_end_date", key: "contract_end_date" },
-  { title: "合約週期", dataIndex: "job_period", key: "job_period" },
-  { title: "N日後到期", dataIndex: "end_day_diff", key: "end_day_diff" },
-  { title: "開工日期", dataIndex: "work_start_date", key: "work_start_date" },
-  { title: "完工日期", dataIndex: "work_end_date", key: "work_end_date" },
-  { title: "跟進人", dataIndex: "buyer_name_zh", key: "buyer_name_zh" },
+  { title: "合約狀態", dataIndex: "status", key: "status" },
+  { title: "合約內容", dataIndex: "content", key: "content" },
+  { title: "合約金額", dataIndex: "tender_amount", key: "tender_amount" },
   { width: "100px", scopedSlots: { customRender: "detail" } },
   { width: "100px", scopedSlots: { customRender: "delete" } }
 ];
 export default {
   data() {
     return {
-      tableData: [
-        {
-          biding_price: "4",
-          bt: "新界屯門海瑞路79號東華大樓14樓物業科",
-          ccn: "佳定物業管理有限公司 ",
-          ccp: "MS ABC",
-          ce: "abc@tungwah.com",
-          cen: "Guardian Property Management Limited ",
-          cf: "98765413",
-          client_data_id: "31",
-          client_id: "29",
-          csn: "c29",
-          ct: "1234 5678",
-          declare_number: "",
-          end_bid_date: "2019-09-04",
-          end_bid_time: "",
-          end_date: "2019-08-21",
-          in_price_date: "2019-08-29",
-          is_bidding: "0",
-          jca: "九龍大南街123號地下",
-          lc: "大南街123號地下",
-          le: "123 Tai Nan Street",
-          lsn: "L00017",
-          min_project: "0",
-          out_price: "5",
-          p_no: "19-c29-(BW)",
-          pl: "short",
-          pmaster_id: "41",
-          project_area: "大南街123",
-          pshort: "short",
-          pt: "",
-          re_bidding_date: "2019-08-27",
-          regulation: "",
-          sales_code: "(BW)",
-          send_bid_date: "2019-08-15",
-          send_bid_way: "",
-          sort: "123",
-          spn_date: "",
-          start_date: "2019-08-01",
-          sub_bid_name: "",
-          sub_bid_number: "",
-          sub_bid_price: "9",
-          sub_price: "3",
-          sub_price_name: "",
-          sub_re_bid_date: "0000-00-00"
-        }
-      ],
+      tableData: [{}],
       dataSource: [],
       columns,
       onTableLoading: false
     };
   },
   created() {
-    //this.getTableData(this.$route.params.bid);
+    this.getTableData();
   },
   methods: {
     onSearch(val) {
       const dataSource = [...this.dataSource];
-      this.tableData = dataSource.filter(item => item.term_contract_id == val);
+      this.tableData = dataSource.filter(
+        item => item.individual_contract_id == val
+      );
       if (val == "") {
         this.tableData = this.dataSource;
       }
     },
     getTableData() {
       this.onTableLoading = true;
-      r_term_contract(this.$route.params.bid)
+      r_individual_contract(this.$route.params.bid)
         .then(res => {
           this.onTableLoading = false;
           this.tableData = res.list;
@@ -146,9 +114,11 @@ export default {
         })
         .catch(err => {});
     },
-    onDelete(term_contract_id) {
-      d_term_contract(term_contract_id)
+    onDelete(individual_contract_id) {
+      console.log(individual_contract_id);
+      d_individual_contract(individual_contract_id)
         .then(res => {
+          console.log(res);
           if (res.status) {
             this.getTableData();
           } else {
@@ -157,7 +127,7 @@ export default {
         .catch(err => {});
     }
   },
-  components: { newtermContract, edittermContract }
+  components: { newindividualContract, editindividualContract }
 };
 </script>
 <style lang="scss">
