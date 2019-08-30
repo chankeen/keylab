cf68501b3a242a468838f1321d9d977009dc74ed
keenchan <keen@ormediagroup.com>
2019-08-30 11:09:56 +0800

    file upload fail

--- src/view/termContract/individualContract.vue
+++ src/view/termContract/individualContract.vue
@@ -15,6 +15,15 @@
             <a-input v-model="info.term_contract_id"></a-input>
           </p>
           <p class="item">
+            <span class="label">合約種類</span>
+            <a-select v-model="info.type">
+              <a-select-option value="冷氣">冷氣</a-select-option>
+              <a-select-option value="電梯">電梯</a-select-option>
+              <a-select-option value="採購">採購</a-select-option>
+              <a-select-option value="其他">其他</a-select-option>
+            </a-select>
+          </p>
+          <p class="item">
             <span class="label">招標日期</span>
             <a-date-picker format="DD/MM/YYYY" v-model="info.tender_start"></a-date-picker>
           </p>
@@ -24,18 +33,18 @@
           </p>
           <p class="item">
             <span class="label">合約金額</span>
-            <a-input v-model="info.unit"></a-input>
+            <a-input v-model="info.tender_amount"></a-input>
           </p>
           <p class="item">
             <span class="label">招標內容</span>
-            <a-input v-model="info.unit"></a-input>
+            <a-input v-model="info.content"></a-input>
           </p>
           <p class="item">
-          <span class="label">招標檔案</span>
-          <span style="text-align:left;width:100%">
-            <uploadFile v-model="info.floor_plan_file"></uploadFile>
-          </span>
-        </p>
+            <span class="label">招標檔案</span>
+            <span style="text-align:left;width:100%">
+              <uploadFile v-model="info.contract_file"></uploadFile>
+            </span>
+          </p>
         </a-col>
       </a-row>
 
@@ -48,13 +57,19 @@
 <script>
 import moment from "moment";
 import uploadFile from "@/components/uploadFile.vue";
-import { u_unit_list } from "@/api/unit_list";
+import { c_individual_contract } from "@/api/individual_contract";
 export default {
   data() {
     return {
       visible: false,
       onSubmiting: false,
-      info: {}
+      info: {
+        term_contract_id: 0,
+        type: "",
+        tender_start: "",
+        tender_end: "",
+        tender_amount: ""
+      }
     };
   },
   components: { uploadFile },
@@ -70,18 +85,40 @@ export default {
     onClose() {
       this.visible = false;
     },
+    get_file_info(item) {
+      //get info of file
+      item.forEach(value => {
+        for (var key in value) {
+          if (
+            key == "name" ||
+            key == "url" ||
+            key == "uid" ||
+            key == "status"
+          ) {
+            continue;
+          }
+          delete value[key];
+        }
+      });
+      return item;
+    },
     onSubmit() {
       for (const key in this.info) {
         if (this.info.hasOwnProperty(key)) {
-          if (typeof this.info[key] == "object") {
+          if (
+            typeof this.info[key] == "object" &&
+            !Array.isArray(this.info[key])
+          ) {
             this.info[key] = this.info[key]._isValid
               ? this.info[key].format("YYYY-MM-DD")
               : "";
           }
         }
       }
+      this.info.property_id = this.$route.params.bid;
+      this.info.contract_file = this.get_file_info(this.info.contract_file);
       this.onSubmiting = true;
-      u_unit_list(this.info)
+      c_individual_contract(this.info)
         .then(res => {
           if (res.status) {
             this.$message.success("成功添加");
