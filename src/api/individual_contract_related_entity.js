ca44b7aba05629638fec711617b1d07ca10539aa
keenchan <keen@ormediagroup.com>
2019-10-01 02:47:27 +0800

    ind. contract api added

--- src/api/individual_contract_related_entity.js
+++ src/api/individual_contract_related_entity.js
@@ -1,39 +1,39 @@
 import request from "@/utils/request.js"
 import Qs from "qs"
 
-export function c_term_contract_related_entity(values) {//分判商邀請函
+export function c_individual_contract_related_entity(values) {//分判商邀請函
     return request({
-        url: "api/data?page=term_contract_related_entity&action=c_term_contract_related_entity",
+        url: "api/data?page=individual_contract_related_entity&action=c_individual_contract_related_entity",
         method: "POST",
         data: Qs.stringify({
             ...values
         })
     })
 }
-export function r_term_contract_related_entity(term_contract_id) {//報價單
+export function r_individual_contract_related_entity(individual_contract_id) {//報價單
     return request({
-        url: "api/data?page=term_contract_related_entity&action=r_term_contract_related_entity",
+        url: "api/data?page=individual_contract_related_entity&action=r_individual_contract_related_entity",
         method: "POST",
         data: Qs.stringify({
-            term_contract_id
+            individual_contract_id
         })
     })
 }
-export function u_term_contract_related_entity(values) {//報價單
+export function u_individual_contract_related_entity(values) {//報價單
     return request({
-        url: "api/data?page=term_contract_related_entity&action=u_term_contract_related_entity",
+        url: "api/data?page=individual_contract_related_entity&action=u_individual_contract_related_entity",
         method: "POST",
         data: Qs.stringify({
             ...values
         })
     })
 }
-export function d_term_contract_related_entity(term_contract_related_entity_id) {//報價單
+export function d_individual_contract_related_entity(individual_contract_related_entity_id) {//報價單
     return request({
-        url: "api/data?page=term_contract_related_entity&action=d_term_contract_related_entity",
+        url: "api/data?page=individual_contract_related_entity&action=d_individual_contract_related_entity",
         method: "POST",
         data: Qs.stringify({
-            term_contract_related_entity_id
+            individual_contract_related_entity_id
         })
     })
 }
--- src/api/individual_contract_work_report.js
+++ src/api/individual_contract_work_report.js
@@ -1,40 +1,40 @@
 import request from "@/utils/request.js"
 import Qs from "qs"
 
-export function c_term_contract_regular_report(values) {//分判商邀請函
+export function c_individual_contract_work_report(values) {//分判商邀請函
     console.log(values);
     return request({
-        url: "api/data?page=term_contract_regular_report&action=c_term_contract_regular_report",
+        url: "api/data?page=individual_contract_work_report&action=c_individual_contract_work_report",
         method: "POST",
         data: Qs.stringify({
             ...values
         })
     })
 }
-export function r_term_contract_regular_report(term_contract_id) {//報價單
+export function r_individual_contract_work_report(individual_contract_id) {//報價單
     return request({
-        url: "api/data?page=term_contract_regular_report&action=r_term_contract_regular_report",
+        url: "api/data?page=individual_contract_work_report&action=r_individual_contract_work_report",
         method: "POST",
         data: Qs.stringify({
-            term_contract_id
+            individual_contract_id
         })
     })
 }
-export function u_term_contract_regular_report(values) {//報價單
+export function u_individual_contract_work_report(values) {//報價單
     return request({
-        url: "api/data?page=term_contract_regular_report&action=u_term_contract_regular_report",
+        url: "api/data?page=individual_contract_work_report&action=u_individual_contract_work_report",
         method: "POST",
         data: Qs.stringify({
             ...values
         })
     })
 }
-export function d_term_contract_regular_report(regular_report_id) {//報價單
+export function d_individual_contract_work_report(work_report_id) {//報價單
     return request({
-        url: "api/data?page=term_contract_regular_report&action=d_term_contract_regular_report",
+        url: "api/data?page=individual_contract_work_report&action=d_individual_contract_work_report",
         method: "POST",
         data: Qs.stringify({
-            regular_report_id
+            work_report_id
         })
     })
 }
